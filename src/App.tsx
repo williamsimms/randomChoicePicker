0453183b3411cc63c96e93968e500cc55e75e5c7
williamsimms <williamsimms193@gmail.com>
2020-12-10 12:23:11 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.scss'
 const App = () => {
   return (
     <div className='app'>
-      <h1>App</h1>
+      <h3>
+        Enter all of the choices divided by a comma (','). <br /> Press enter when you're done{' '}
+      </h3>
+      <textarea placeholder='Enter Choices here' cols={30} rows={10}></textarea>
+      <div className='tags'></div>
     </div>
   )
 }
