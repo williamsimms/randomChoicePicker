2eb4e7e6c266e7667905b423fb6ecfb92c86258d
williamsimms <williamsimms193@gmail.com>
2020-12-10 13:00:48 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.scss'
 
 const App = () => {
+  const textAreaRef = useRef<any>()
+
+  useEffect(() => {
+    textAreaRef.current.focus()
+  }, [])
+
+  const [textArea, setTextArea] = useState('')
+  const [tags, setTags] = useState([])
+
+  const onKeyUp = (e: any) => {
+    if (e.keyCode === 13) {
+      const arr: any = textArea
+        .trim()
+        .split(',')
+        .map((tag) => {
+          return tag.trim()
+        })
+
+      setTags(arr)
+      setTextArea('')
+    }
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextArea(e.target.value)
+  }
+
+  const pickTag = () => {}
+
   return (
     <div className='app'>
       <h3>
-        Enter all of the choices divided by a comma (','). <br /> Press enter when you're done{' '}
+        Enter all of the choices divided by a comma (','). <br /> Press enter when you're done.
       </h3>
-      <textarea placeholder='Enter Choices here' cols={30} rows={10}></textarea>
-      <div className='tags'></div>
+
+      <textarea
+        placeholder='Enter Choices here'
+        cols={30}
+        rows={10}
+        onKeyUp={onKeyUp}
+        value={textArea}
+        onChange={onChange}
+        ref={textAreaRef}></textarea>
+
+      <div className='tags'>
+        {tags.map((tag, index) => (
+          <p className='tag' key={index}>
+            {tag}
+          </p>
+        ))}
+      </div>
     </div>
   )
 }
